061f161e8f4c895d5c87e2f80476ca0b5d92c0df
Ukyo Saito <>
2019-07-15 16:59:55 +0900

    prepare files for heroku

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
 from app.app import app
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/tutorial_blog'
+db_uri = os.environ.get('DATABASE_URL') or 'postgresql://postgres:@localhost/tutorial_blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
