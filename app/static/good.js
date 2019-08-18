a959bc8d8430f8a5102f56e96764e005194beebc
“kiyokiyo“ <kiyokiyo.kzsby@gmail.com>
2019-08-18 13:45:16 +0900

    use vscode

--- app/static/good.js
+++ app/static/good.js
@@ -1,21 +1,22 @@
-
-$(function(){
-    var $good = $('.good-btn'),
-                contentId;
-    $good.on('click',function(e){
-        e.stopPropagation();
-        var $this = $(this);
-        var data = JSON.stringify({"content_id":$this.data('content_id')});
-        $.ajax({
-            type: 'POST',
-            url: '/good',
-            data: data,
-            contentType:'application/json'
-        }).done(function(data){
-            $this.next().text(data);
-            $this.toggleClass('good-btn-active');
-        }).fail(function(msg) {
-            console.log('Ajax Error');
-        });
-    });
+$(function() {
+  var $good = $(".good-btn"),
+    contentId;
+  $good.on("click", function(e) {
+    e.stopPropagation();
+    var $this = $(this);
+    var data = JSON.stringify({ content_id: $this.data("content_id") });
+    $.ajax({
+      type: "POST",
+      url: "/good",
+      data: data,
+      contentType: "application/json"
+    })
+      .done(function(data) {
+        $this.next().text(data);
+        $this.toggleClass("good-btn-active");
+      })
+      .fail(function(msg) {
+        console.log("Ajax Error");
+      });
+  });
 });
